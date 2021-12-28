5db5999f9f682ab03aedf0c7f1ad5e767a815ee7
fardeen9784 <83347942+fardeen9784@users.noreply.github.com>
2021-12-28 23:27:03 +0530

    1st build

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing", {
+    strings: ["Dark Arts, Dark Culture is the Origin....... :(", "In Sketching from the Imagination...... :)", "People Likes Dark Color Except human Faces.. :("],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true
+});
+var typed = new Typed(".typing2", {
+    strings: ["a Graphics Designer.", "a Web Developer.","an Artist.","a Btech Student."],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true
+});
